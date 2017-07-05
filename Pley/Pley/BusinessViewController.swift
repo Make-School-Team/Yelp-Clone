b3d0ae0e80b4002543eb1069a038998cfdc6666b
Claudiazhaoya <sun515@purdue.edu>
2017-07-05 16:31:42 -0700

    BusinessViewController

---
+++ Pley/Pley/BusinessViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BusinessViewController.swift
+//  Pley
+//
+//  Created by Zhaoya Sun on 7/5/17.
+//  Copyright © 2017 Chenyang Zhang. All rights reserved.
+//
+
+//
+//  ViewController.swift
+//  Pley
+//
+//  Created by Chenyang Zhang on 7/5/17.
+//  Copyright © 2017 Chenyang Zhang. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class BusinessViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
