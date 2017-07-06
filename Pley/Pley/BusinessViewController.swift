4f8216bc64f3c4a78191595cb10cfb656f9b7fff
Claudiazhaoya <sun515@purdue.edu>
2017-07-06 10:23:06 -0700

    businessViewController changed

--- Pley/Pley/BusinessViewController.swift
+++ Pley/Pley/BusinessViewController.swift
@@ -87,22 +87,6 @@ class BusinessViewController: UITableViewController {
         }
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return businesses.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
-        
-        return cell
-    }
-
-    
     
     
 }
